80cfa303116cf817eae2624e94952e8b16c2fa19
russmiles <russ@russmiles.com>
2016-11-15 15:54:54 +0000

    Initial commit from the atomist bot

---
+++ src/main/java/com/russmiles/ComplaintsAPIApplication.java
@@ -0,0 +1,12 @@
+package com.russmiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintsAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintsAPIApplication.class, args);
+    }
+}
---
+++ src/main/java/com/russmiles/ComplaintsAPIConfiguration.java
@@ -0,0 +1,9 @@
+package com.russmiles;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ComplaintsAPIConfiguration {
+    // Your beans go here ...
+    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
+}
